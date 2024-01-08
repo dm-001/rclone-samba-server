d07ad8249160e0deeb411566febd03f5bcbfaef8
Dean <108975658+dm-001@users.noreply.github.com>
2024-01-08 22:04:01 +1100

    Update rclone-mount.sh
    Add cache refresh and timer handling variables

--- rclone-mount.sh
+++ rclone-mount.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # Validate env vars and set up rclone S3 mount
 
-if [[ ! -z "${CACHE_MAX}" ]]; then
-		echo "$(date) [info] Max cache size: '${CACHE_MAX}'"
-	else
-		echo "$(date) [warn] Max cache size not defined (via CACHE_MAX). Using default: 10G"
-		export CACHE_MAX="10G"
+if [[ -z "${CACHE_MAX_SIZE}" ]]; then
+		echo "$(date) [warn] VFS max cache size not defined (via CACHE_MAX_SIZE). Using default: 10G"
+		export CACHE_MAX_SIZE="10G"
 fi  
 
-if [[ ! -z "${S3BUCKET}" ]]; then
-		echo "$(date) [info] S3 Bucket: '${S3BUCKET}'"
-	else
+if [[ -z "${CACHE_REFRESH_TIME}" ]]; then
+		echo "$(date) [warn] Cache refresh time not defined (via CACHE_REFRESH TIME). Using default: 30s"
+		export CACHE_REFRESH_TIME="30s"
+fi  
+
+if [[ -z "${S3BUCKET}" ]]; then
 		echo "$(date) [warn] S3 Bucket Name not defined (via S3BUCKET)."
 		export S3BUCKET="1"
 fi  
 
 echo "Mounting S3 bucket via rclone..."
+echo "$(date) [info] S3 Bucket: '${S3BUCKET}'"
+echo "$(date) [info] VFS cache maximum size: '${CACHE_MAX_SIZE}'"
+echo "$(date) [info] Cache refresh time: '${CACHE_REFRESH_TIME}'"
 /usr/bin/rclone --config=/rclone/config/rclone.conf --cache-dir=/rclone/cache mount remote:${S3BUCKET} /mnt/remote \
-                --vfs-cache-mode writes --vfs-cache-max-size ${CACHE_MAX}  --vfs-fast-fingerprint --allow-other --allow-non-empty
+                --vfs-cache-mode full --vfs-cache-max-size ${CACHE_MAX_SIZE} --dir-cache-time ${CACHE_REFRESH_TIME} \
+	       	--vfs-fast-fingerprint --allow-other --allow-non-empty
